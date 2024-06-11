16eff63d3fc6380bbf866a488fb7f7eeeeb6e7ac
Tonyopano <antonyopano@gmail.com>
2024-06-11 23:10:47 +0300

    Configuring convex on the application

---
+++ app/providers/ConvexProvider.tsx
@@ -0,0 +1,13 @@
+"use client";
+import { ReactNode } from "react";
+import { ConvexReactClient } from "convex/react";
+
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
+export default function ConvexClientProvider({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return <ConvexClientProvider>{children}</ConvexClientProvider>;
+}
