129a9229e9a746e362bc0d147aee791694f3467c
Tonyopano <antonyopano@gmail.com>
2024-06-12 08:16:02 +0300

    Configuring convex and clerk keys

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -1,5 +1,4 @@
-
-'use client';
+"use client";
 
 import { sidebarLinks } from "@/constants";
 import { cn } from "@/lib/utils";
@@ -9,8 +8,8 @@ import { usePathname, useRouter } from "next/navigation";
 import React from "react";
 
 const LeftSidebar = () => {
-    const pathname = usePathname();
-    const router = useRouter();
+  const pathname = usePathname();
+  const router = useRouter();
 
   return (
     <section className="left_sidebar">
@@ -26,15 +25,19 @@ const LeftSidebar = () => {
         </Link>
 
         {sidebarLinks.map(({ route, label, imgURL }) => {
-          const isActive = pathname === route || pathname.startsWith(`${route}/`);
+          const isActive =
+            pathname === route || pathname.startsWith(`${route}/`);
 
           return (
             <Link
               href={route}
               key={label}
-              className={cn("flex gap-3 items-center py-4 max-lg:px-4: justify-center lg:justify-start", {
-                'bg-nav-focus border-r-4 border-orange-1': isActive
-              })}
+              className={cn(
+                "flex gap-3 items-center py-4 max-lg:px-4: justify-center lg:justify-start",
+                {
+                  "bg-nav-focus border-r-4 border-orange-1": isActive,
+                }
+              )}
             >
               <Image src={imgURL} alt={label} width={24} height={24} />
               <p>{label}</p>
---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://casual-mullet-17.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
